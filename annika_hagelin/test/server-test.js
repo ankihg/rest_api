d64a98dc9b3c190375df82bbfed639fe68d20472
ankihg <hagelin.annika@gmail.com>
2016-03-17 14:21:33 -0700

    species tests

--- annika_hagelin/test/server-test.js
+++ annika_hagelin/test/server-test.js
@@ -7,6 +7,8 @@ var expect = chai.expect;
 describe('species router testing', () => {
 
   var cedrusDeodaraID;
+  var cedrusAtlanticaID;
+  var memusDeletaID;
 
   it('should post species cedrus deodara', (done) => {
     request('localhost:3000')
@@ -16,6 +18,41 @@ describe('species router testing', () => {
       expect(err).eql(null);
       expect(res).status(200);
       cedrusDeodaraID = res.body._id;
+      done();
+    });
+  });
+
+  it('should post species livermorium lawrencia', (done) => {
+    request('localhost:3000')
+    .post('/speciess')
+    .send({"genus":"livermorium", "species":"lawrencia", "cmnName":"lawrence livermore"})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      cedrusAtlanticaID = res.body._id;
+      done();
+    });
+  });
+
+  it('should post plzus repsonda', (done) => {
+    request('localhost:3000')
+    .post('/speciess')
+    .send({"genus":"plzus", "species":"responda", "cmnName":"respond plz"})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
+
+  it('should post memus deleta', (done) => {
+    request('localhost:3000')
+    .post('/speciess')
+    .send({"genus":"memus", "species":"deleta", "cmnName":"delete me"})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      memusDeletaID = res.body._id;
       done();
     });
   });
@@ -36,10 +73,34 @@ describe('species router testing', () => {
     .end((err, res) => {
       expect(err).eql(null);
       expect(res).status(200);
-      expect(res.body.genus).eql('cedrus');
+      expect(res.body.species).eql('deodara');
       done();
     });
   });
+
+
+  it('should put cedrus atlantic to plzus repsonda', (done) => {
+    request('localhost:3000')
+    .put('/speciess/'+cedrusAtlanticaID)
+    .send({"genus":"cedrus", "species":"atlantica", "cmnName":"atlas cedar"})
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
+
+  it('should delete memus deleta', (done) => {
+    request('localhost:3000')
+    .del('/speciess/'+memusDeletaID)
+    .end((err, res) => {
+      expect(err).eql(null);
+      expect(res).status(200);
+      done();
+    });
+  });
+
+
 
 
 
